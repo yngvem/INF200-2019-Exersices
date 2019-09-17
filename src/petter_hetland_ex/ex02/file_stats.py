c0c99211ad06bc2079984bfe369630141378cd40
Petter Hetland <Pehe@nmbu.no>
2019-09-18 00:05:29 +0200

    Started using Gitkraken
    Small changes

---
+++ src/petter_hetland_ex/ex02/file_stats.py
@@ -0,0 +1,22 @@
+
+__author__ = "petter hetland"
+__email__ = "pehe@nmbu.no"
+
+
+def char_counts(textfilename):
+    result = []
+    infile = str(open(textfilename, 'r'))
+    for letter in infile:
+        result[letter] = infile.count(letter)
+    return infile
+
+
+if __name__ == '__main__':
+
+    filename = 'file_stats.py'
+    frequencies = char_counts(filename)
+    for code in range(256):
+        if frequencies[code] > 0:
+            print('{:3}{:>4}{:6}'.format(code,
+                                         chr(code) if code >= 32 else '',
+                                         frequencies[code]))
